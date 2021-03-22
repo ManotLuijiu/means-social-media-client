9f60390586383f9a220c1bb0cd4d169ea1cdc809
Manot Luijiu <manot.luijiu@gmail.com>
2021-03-22 20:28:36 +0700

    finished register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
+import { Form, Button, Header } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-export default function Register() {
+export default function Register(props) {
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  console.log(values);
+  const enabled =
+    values.username.length > 0 &&
+    values.email.length > 0 &&
+    values.password.length > 0 &&
+    values.confirmPassword.length > 0;
+  console.log(enabled);
+
+  const REGISTER_USER = gql`
+    mutation register(
+      $username: String!
+      $email: String!
+      $password: String!
+      $confirmPassword: String!
+    ) {
+      register(
+        registerInput: {
+          username: $username
+          email: $email
+          password: $password
+          confirmPassword: $confirmPassword
+        }
+      ) {
+        id
+        email
+        username
+        createdAt
+        token
+      }
+    }
+  `;
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const [addUser, { loading, error }] = useMutation(REGISTER_USER, {
+    update(_, result) {
+      console.log(result);
+      props.history.push('/');
+    },
+    onError(err) {
+      console.log(err);
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addUser();
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <section className="text-gray-600 body-font relative">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+          <Header
+            as="h1"
+            className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Register
+          </Header>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Please enter all fields</p>
+        </div>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 w-full">
+              <div className="relative">
+                <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
+                  <Form.Input
+                    label="Username"
+                    placeholder="Username"
+                    name="username"
+                    type="text"
+                    value={values.username}
+                    error={!!errors.username}
+                    onChange={onChange}
+                  />
+                  <Form.Input
+                    label="Email"
+                    placeholder="Email"
+                    name="email"
+                    type="email"
+                    value={values.email}
+                    error={!!errors.email}
+                    onChange={onChange}
+                  />
+                  <Form.Input
+                    label="Password"
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    error={!!errors.password}
+                    onChange={onChange}
+                  />
+                  <Form.Input
+                    label="Confirm Password"
+                    placeholder="Confirm Password"
+                    name="confirmPassword"
+                    type="password"
+                    value={values.confirmPassword}
+                    error={!!errors.confirmPassword}
+                    onChange={onChange}
+                  />
+                  <Button type="submit" primary>
+                    Register
+                  </Button>
+                </Form>
+                {error && (
+                  <p className="prose text-red-600 text-base mt-2">
+                    Error Please fix these then try again
+                  </p>
+                )}
+                {Object.keys(errors).length > 0 && (
+                  <div className="ui error message">
+                    <ul className="list">
+                      {Object.values(errors).map((value) => (
+                        <li key={value}>{value}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+                {loading && <p>Loading...</p>}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
+
+Register.propTypes = {
+  history: PropTypes.string.isRequired,
+};
